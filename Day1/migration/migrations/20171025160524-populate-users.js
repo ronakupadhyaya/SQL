8aba81691bc30a2cdba81b5f079f70f43b11fe78
Tom Fulton <TFulton42@gmail.com>
2017-10-25 12:07:17 -0400

    Modified

--- Day1/migration/migrations/20171025160524-populate-users.js
+++ Day1/migration/migrations/20171025160524-populate-users.js
@@ -19,7 +19,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20171023230131-add-users-up.sql');
+  var filePath = path.join(__dirname, 'sqls', '20171025160524-populate-users-up.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
@@ -34,7 +34,7 @@ exports.up = function(db) {
 };
 
 exports.down = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20171023230131-add-users-down.sql');
+  var filePath = path.join(__dirname, 'sqls', '20171025160524-populate-users-down.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
