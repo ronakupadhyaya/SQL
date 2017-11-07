ed75428bd3c161cfa1f3e8145d2bccae8544ad94
jeff <tangj1122@gmail.com>
2017-11-07 11:39:45 -0800

    finish normalization school

--- day2/normalize/migrations/sqls/20171107055029-init-up.sql
+++ day2/normalize/migrations/sqls/20171107055029-init-up.sql
@@ -21,3 +21,19 @@ CREATE TABLE courses (
   course_name TEXT,
   students TEXT
 );
+
+-- normalize professors
+ALTER TABLE professors DROP COLUMN email_id;
+ALTER TABLE professors ADD COLUMN email TEXT;
+ALTER TABLE professors DROP COLUMN course_id;
+ALTER TABLE professors RENAME COLUMN professor_name TO name;
+DROP TABLE professor_emails;
+
+-- normalize students
+ALTER TABLE students ALTER COLUMN student_name SET DATA TYPE TEXT;
+ALTER TABLE students RENAME COLUMN student_name TO name;
+ALTER TABLE students DROP COLUMN courses;
+
+-- normalize courses
+ALTER TABLE courses DROP COLUMN students;
+ALTER TABLE courses RENAME COLUMN course_name TO name;
