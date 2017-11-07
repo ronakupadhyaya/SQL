ae892b5b78ebbf8f45943be4711f9aa79c2cdd3f
masonliyu <masonliyu@gmail.com>
2017-11-07 15:10:11 -0800

    pg exercises

--- day2/pg/query.js
+++ day2/pg/query.js
@@ -1,3 +1,41 @@
 "use strict";
 
-// YOUR CODE HERE
+const pool = require('./pool');
+
+const generateTable = `
+    CREATE TABLE animals(
+        name    TEXT,
+        food    TEXT,
+        sound   TEXT
+    );
+`;
+
+const insertAnimal = (name, food, sound) => (
+  `
+    INSERT INTO animals
+            (name, food, sound)
+        VALUES
+            ('${name}', '${food}', '${sound}');
+    `
+);
+
+(async () => {
+  try {
+    // await pool.query(generateTable);
+    const fullInsert = insertAnimal('donkey', 'carrots', 'hee-haw') +
+        insertAnimal('cow', 'grass', 'moo') + insertAnimal('duck', 'seeds', 'quack');
+    await pool.query(fullInsert);
+    const searchMoo = `
+        SELECT
+            a.name
+        FROM 
+            animals a
+        WHERE
+            a.sound = 'moo'
+    `;
+    const result = await pool.query(searchMoo);
+    console.log('The animal name is: ', result.rows[0].name);
+  } catch(err) {
+    console.error(err);
+  }
+})();
