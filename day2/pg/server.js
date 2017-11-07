eb0fd392013a7fbc77f1ad397b6927b4f0db9a53
jeff <tangj1122@gmail.com>
2017-11-07 13:32:15 -0800

    fin animals

--- day2/pg/server.js
+++ day2/pg/server.js
@@ -14,6 +14,39 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 // YOUR CODE HERE
+const pool = require('./pool');
+
+app.get('/animals', (req, res) => {
+  if (!req.query.name) {
+    pool.query('SELECT name, sound FROM animals')
+    .then((animals) => {
+      res.json(animals.rows);
+    });
+  } else {
+    pool.query(`SELECT name, sound FROM animals WHERE name=$1`, [req.query.name])
+    .then(animals => {
+      res.json(animals.rows);
+    });
+  }
+});
+
+app.get('/delete/:name', (req, res) => {
+  pool.query(`DELETE FROM animals WHERE name=$1`, [req.params.name])
+  .then(resp => {
+    res.json({success: true});
+  })
+  .catch(err => res.json({err}));
+});
+
+app.post('/create', (req, res) => {
+  const query = {
+    text: 'INSERT INTO animals(name, food, sound) VALUES($1, $2, $3)',
+    values: [req.body.name, req.body.food, req.body.sound]
+  };
+  pool.query(query)
+  .then(resp => res.json({success: true}))
+  .catch(err => res.json({err}));
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
