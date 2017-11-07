dfd117328d39d0f77c6e688f32bf3b60d6603938
Emma Paschke <epaschke>
2017-11-07 10:33:02 -0800

    day1

---
+++ day1/migrate/migrations/sqls/20171106191948-status-column-up.sql
@@ -0,0 +1,3 @@
+alter table users add column status varchar;
+update users set status = 'surfing' where state = 'CA';
+update users set status = 'working' where not state = 'CA';
---
+++ day1/migrate/migrations/sqls/20171106192645-populate-planets-up.sql
@@ -0,0 +1,5 @@
+INSERT INTO planets VALUES (DEFAULT, 'Mercury', 0);
+INSERT INTO planets VALUES (DEFAULT, 'Venus', 0);
+INSERT INTO planets VALUES (DEFAULT, 'Earth', 1);
+INSERT INTO planets VALUES (DEFAULT, 'Mars', 2);
+INSERT INTO planets VALUES (DEFAULT, 'Jupiter', 69);
