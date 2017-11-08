87d46630d27d0eb55eeb37db7637c6c9f457cbaf
jeff <tangj1122@gmail.com>
2017-11-08 09:39:21 -0800

    redo query with async/await

--- day2/pg/query.js
+++ day2/pg/query.js
@@ -3,25 +3,57 @@
 // YOUR CODE HERE
 const pool = require('./pool');
 
+// ASYNC
+async function doAnimalQueries() {
 
-pool.query(`DROP TABLE animals`)
-.then(() => {
-  pool.query(`CREATE TABLE animals(
-    id serial PRIMARY KEY,
-    name TEXT,
-    food TEXT,
-    sound TEXT
-  )`)
-  .then(() => {
-    pool.query(`INSERT INTO animals
-      (name, food, sound)
-      VALUES
-        ('donkey', 'carrots', 'hee-haw'),
-        ('cow', 'grass', 'moo'),
-        ('duck', 'seeds', 'quack')
+  try {
+    await pool.query(`DROP TABLE animals`);
+    await pool.query(`
+      CREATE TABLE animals(
+        id serial PRIMARY KEY,
+        name TEXT,
+        food TEXT,
+        sound TEXT
+      )
     `);
-  })
-  .then(() => pool.query(`SELECT * FROM animals WHERE sound='moo'`))
-  .then(res=> console.log(`The ${res.rows[0].name} goes ${res.rows[0].sound}`));
-})
-.catch(err => console.log(err));
+    const res = await pool.query(`
+      INSERT INTO animals
+        (name, food, sound)
+        VALUES
+          ('donkey', 'carrots', 'hee-haw'),
+          ('cow', 'grass', 'moo'),
+          ('duck', 'seeds', 'quack')
+      `
+    );
+    console.log(res);
+  } catch (err) {
+    console.log(err.stack);
+  }
+
+}
+
+doAnimalQueries();
+
+// PROMISES
+// pool.query(`DROP TABLE animals`)
+// .then(() => {
+//   pool.query(`CREATE TABLE animals(
+//     id serial PRIMARY KEY,
+//     name TEXT,
+//     food TEXT,
+//     sound TEXT
+//   )`)
+//   .then(() => {
+//     pool.query(`INSERT INTO animals
+//       (name, food, sound)
+//       VALUES
+//         ('donkey', 'carrots', 'hee-haw'),
+//         ('cow', 'grass', 'moo'),
+//         ('duck', 'seeds', 'quack')
+//     `);
+//   })
+//   .then(() => pool.query(`SELECT * FROM animals WHERE sound='moo'`))
+//   .then(res=> console.log(`The ${res.rows[0].name} goes ${res.rows[0].sound}`));
+// })
+// .catch(err => console.log(err));
+
